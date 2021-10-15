c519ebc8709b1e1deec8562ff07be797d48b962b
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-16 00:26:48 +0530

    start

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bl.addressbook;
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("**********Welcome to Address Book Program***********");
+
+	}
+
+}
